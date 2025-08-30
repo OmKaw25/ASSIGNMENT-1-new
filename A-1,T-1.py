6d89b2fc820e7014bdbb9dccafd0874c14d63c99
OmKaw25 <wakmo32@gmail.com>
2025-08-30 21:09:05 +0530

    Add files via upload

---
+++ A-1,T-1.py
@@ -0,0 +1,10 @@
+inp1=int(input('Enter first number: '))
+inp2=int(input('Enter second number: '))
+a=inp1+inp2
+b=inp1-inp2
+c=inp1*inp2
+d=inp1/inp2
+print('Addition:',a)
+print('Subtraction:',b)
+print('Multiplication:',c)
+print('Division:',d)
---
+++ A-1,T-2.py
@@ -0,0 +1,4 @@
+fname=input('Enter first name:')
+lname=input('Enter last name:')
+fullname=fname+' '+lname
+print('Hello,',fullname,'! Welcome to the python program')
